212bc26f8afc32ce90bc4238ce7ad18a55950779
Jt054270 <jessieturks33@gmail.com>
2023-06-30 17:58:06 -0700

    updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,21 +2,20 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of data elements in a linked list.
+ * list_len - returns tha number of elements in a linked list
  * @w: pointer to the list_t list
  *
- * Return: number of data elements in w
+ * Return: Npnumber of elements in w
  */
 size_t list_len(const list_t *w)
 {
-    size_t data_count = 0;
+	size_t count = 0;
 
-    while (w)
-    {
-        data_count++;
-        w = w->next;
-    }
+	while (w)
+	{
+		count++;
+		w = w->next;
+	}
 
-    return data_count;
+	return (count);
 }
-
