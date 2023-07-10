e5b958d665386948ae6455ca4ea579b809752283
Jt054270 <jessieturks33@gmail.com>
2023-07-10 15:53:45 -0700

    FILE

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file being read.
- * @letters: Number of letters to be read.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: Number of bytes read and printed.
- *         0 when function fails or filename is NULL.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	char *buf;
-	ssize_t fd, w, t;
+	int fd, w, len = 0;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+	if (filename == NULL)
+		return (-1);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (0);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	free(buf);
+	if (fd == -1 || w == -1)
+		return (-1);
+
 	close(fd);
 
-	return (w);
+	return (1);
 }
